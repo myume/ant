9f4201712fd81137e43cb74bd1e7a7dd7c406f65
yum <contact@meyume.com>
2026-02-12 12:50:11 -0500

    feat(plugin): display annotations

--- plugin/src/extension.ts
+++ plugin/src/extension.ts
@@ -1,25 +1,92 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+import { spawnSync } from "child_process";
 import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "ant" is now active!');
+type Annotation = {
+  annotation: string;
+  source: string;
+  row: number;
+};
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand("ant.helloWorld", () => {
-    // The code you place here will be executed every time your command is executed
-    // Display a message box to the user
-    vscode.window.showInformationMessage("Hello World from ant!");
+type State = {
+  toggled: boolean;
+};
+
+const annotationText = vscode.window.createTextEditorDecorationType({
+  after: {
+    color: new vscode.ThemeColor("editorGhostText.foreground"),
+    margin: "0 0 0 1em",
+  },
+});
+
+function getTruncatedText(text: string, maxLength: number): string {
+  if (text.length <= maxLength) return text;
+  return text.substring(0, maxLength - 3) + "...";
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  const state: State = { toggled: false };
+  const config = vscode.workspace.getConfiguration("ant");
+
+  const disposable = vscode.commands.registerCommand("ant.toggle", () => {
+    state.toggled = !state.toggled;
+
+    const editor = vscode.window.activeTextEditor;
+    if (editor) {
+      if (!state.toggled) {
+        editor.setDecorations(annotationText, []);
+        return;
+      }
+
+      editor.setDecorations(
+        annotationText,
+        getAnnotations(config.get("binaryPath")).map((annotation) => {
+          const line = editor.document.lineAt(annotation.row - 1);
+          const range = new vscode.Range(line.range.end, line.range.end);
+          return {
+            range,
+            hoverMessage: new vscode.MarkdownString(annotation.annotation),
+            renderOptions: {
+              after: {
+                contentText: getTruncatedText(annotation.annotation, 80),
+              },
+            },
+          };
+        }),
+      );
+    }
   });
 
   context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
+
+const getAnnotations = (binaryPath?: string): Annotation[] => {
+  const antDir = vscode.workspace.workspaceFolders?.at(0)?.uri.fsPath;
+  const file = vscode.window.activeTextEditor?.document.fileName;
+  if (!antDir) {
+    vscode.window.showInformationMessage("Missing root folder");
+    return [];
+  }
+  if (!file) {
+    vscode.window.showInformationMessage("No active editor");
+    return [];
+  }
+
+  const ant = spawnSync(binaryPath ?? "ant", [
+    "-o",
+    antDir,
+    "list",
+    file,
+    "--json",
+  ]);
+  if (ant.error) {
+    console.error(ant.error);
+  }
+  if (ant.status != 0) {
+    vscode.window.showInformationMessage(ant.stdout.toString());
+    return [];
+  }
+
+  return JSON.parse(ant.stdout.toString());
+};
